3048aa2b5d5facb6aa0d66c1dcf6e62ea00ebb47
Karim Mansour <senegalo@gmail.com>
2013-03-27 15:15:30 +0200

    file reformatting

--- public/js/dialogs/components-delete.js
+++ public/js/dialogs/components-delete.js
@@ -1,6 +1,6 @@
-(function($){    
+(function($) {
     var currentCallback, theDialog;
-    $(function(){
+    $(function() {
         theDialog = mxBuilder.dialogs.flexlyDialog.create({
             modal: true,
             draggable: false,
@@ -9,26 +9,26 @@
             autoOpen: false,
             title: "Delete Confirmation",
             buttons: [{
-                label: "Cancel",
-                klass: "flexly-icon-close-big-black",
-                click: function click(){
-                    $(this).dialog("close");
-                }
-            },{
-                label: "OK",
-                klass: "flexly-icon-check-big-black",
-                click: function click(event){
-                    currentCallback.call(this,event);
-                    $(this).dialog("close");
-                }
-            }]
+                    label: "Cancel",
+                    klass: "flexly-icon-close-big-black",
+                    click: function click() {
+                        $(this).dialog("close");
+                    }
+                }, {
+                    label: "OK",
+                    klass: "flexly-icon-check-big-black",
+                    click: function click(event) {
+                        currentCallback.call(this, event);
+                        $(this).dialog("close");
+                    }
+                }]
         });
     });
-    mxBuilder.dialogs.deleteDialog = function(obj){
+    mxBuilder.dialogs.deleteDialog = function(obj) {
         obj.msg = obj.msg ? obj.msg : "Please confirm the delete operation.";
         theDialog.find(".flexly-dialog-content").html(obj.msg);
         currentCallback = obj.callback;
         theDialog.dialog("open");
-    }
-    
-}(jQuery))
+    };
+
+}(jQuery));
