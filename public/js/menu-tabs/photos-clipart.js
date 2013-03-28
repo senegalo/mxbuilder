08a7094d9ef54dfb8c8133de9b833b3a3cc5637f
Karim Mansour <senegalo@gmail.com>
2013-03-28 17:10:58 +0200

    Fixes #51

--- public/js/menu-tabs/photos-clipart.js
+++ public/js/menu-tabs/photos-clipart.js
@@ -24,12 +24,23 @@
                 var theContent = this._template.clone();
                 var listContainer = theContent.find("ul");
                 var draggables = "";
+                
                 for (var i = 61440; i < 61717; i++) {
-                    if (i === 61455 || i === 61471 || i === 61472) {
+                    if (i !== 61487 && (i+1) % 16 === 0) {
                         continue;
                     }
                     draggables += '<li class="clipart mx-helper" data-id="' + i + '">&#' + i + '</li>';
+
+                    switch (i) {
+                        case 61470:
+                            i = 61472;
+                            break;
+                        case 61618:
+                            i = 61631;
+                            break;
+                    }
                 }
+                
                 listContainer.append(draggables);
                 listContainer.on({
                     mouseenter: function mouseenter() {
