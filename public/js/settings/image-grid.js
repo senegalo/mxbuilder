8c37d4651d7ccc0adccbba295d414cb53290494a
Karim Mansour <senegalo@gmail.com>
2013-04-24 15:02:24 +0200

    file formatting and refactoring

--- public/js/settings/image-grid.js
+++ public/js/settings/image-grid.js
@@ -1,101 +1,101 @@
-(function($){
-    $(function(){
+(function($) {
+    $(function() {
         mxBuilder.layout.settingsPanels.imageGrid = {
             //update the template variable
             _template: mxBuilder.layout.templates.find(".image-grid-settings").remove(),
-            _settingsTab : mxBuilder.menuManager.menus.componentSettings,
-            getPanel: function(expand){
+            _settingsTab: mxBuilder.menuManager.menus.componentSettings,
+            getPanel: function(expand) {
                 var imageGrid = this;
                 var thePanel = mxBuilder.layout.utils.getCollapsablePanel(expand);
-                
+
                 //change settings panel title
                 thePanel.find(".flexly-collapsable-title").text("Grid Gallery Settings");
-                
+
                 var theInstance = this._template.clone();
-                
+
                 //fill in all the controls 
                 var controls = {
                     columns: theInstance.find("#image-grid-columns"),
                     spacing: theInstance.find("#image-grid-spacing")
                 };
-                
-                
+
+
                 //Configure the controls here
                 this.applyToSelectionOn(controls, "columns", "input");
                 this.applyToSelectionOn(controls, "spacing", "input");
-                
+
                 this._settingsTab.monitorChangeOnControls(controls);
                 var originalSettings = {};
-                
+
                 //define component properties to add to the original settings object
-                var properties = ["spacing","cols"];
-                
+                var properties = ["spacing", "cols"];
+
                 var firstPass = true;
-                mxBuilder.selection.each(function(){
+                mxBuilder.selection.each(function() {
                     var theSettings = this.getSettings();
-                    for(var p in properties){
-                        if(firstPass){
+                    for (var p in properties) {
+                        if (firstPass) {
                             originalSettings[properties[p]] = theSettings[properties[p]];
                         }
                         var data = theSettings[properties[p]];
-                        if (originalSettings[properties[p]] !== data){
+                        if (originalSettings[properties[p]] !== data) {
                             originalSettings[properties[p]] = false;
                         }
                     }
                     firstPass = false;
                 });
-                
-                this.setValues(controls,originalSettings);
-                
+
+                this.setValues(controls, originalSettings);
+
                 thePanel.on({
-                    previewEnabled: function(){
+                    previewEnabled: function() {
                         imageGrid.applyToSelection(controls);
                         mxBuilder.selection.revalidateSelectionContainer();
                     },
-                    save: function(){
+                    save: function() {
                         imageGrid.applyToSelection(controls);
                         mxBuilder.menuManager.closeTab();
                         mxBuilder.selection.revalidateSelectionContainer();
                     },
-                    previewDisabled: function(){
+                    previewDisabled: function() {
                         mxBuilder.selection.revalidateSelectionContainer();
                     },
-                    cancel: function(){
+                    cancel: function() {
                         mxBuilder.selection.revalidateSelectionContainer();
                         mxBuilder.menuManager.closeTab();
                     }
-                });                
-                
+                });
+
                 thePanel.find(".flexly-collapsable-content").append(theInstance);
                 return thePanel;
             },
-            setValues: function(controls, values){    
+            setValues: function(controls, values) {
                 //implement the setValue function
-                if(values.cols !== false){
+                if (values.cols !== false) {
                     controls.columns.val(values.cols);
                 } else {
                     controls.columns.val('');
                 }
-                
-                if(values.spacing !== false){
+
+                if (values.spacing !== false) {
                     controls.spacing.val(values.spacing);
                 } else {
                     controls.spacing.val('');
                 }
             },
-            applyToSelection: function(controls,values){
-                if(typeof values === "undefined"){
+            applyToSelection: function(controls, values) {
+                if (typeof values === "undefined") {
                     //if no values passed how to do we get the values ?
                     values = {};
-                    if(this._settingsTab.hasChanged(controls.columns)){
+                    if (this._settingsTab.hasChanged(controls.columns)) {
                         //fill up the values array
                         values.cols = controls.columns.val();
                     }
-                    if(this._settingsTab.hasChanged(controls.spacing)){
+                    if (this._settingsTab.hasChanged(controls.spacing)) {
                         values.spacing = controls.spacing.val();
                     }
                 }
-                mxBuilder.selection.each(function(){
+                mxBuilder.selection.each(function() {
                     //apply the values to the selection
                     this.setSpacing(values.spacing);
                     this.setColumns(values.cols);
@@ -103,18 +103,18 @@
                     this.revalidate();
                 });
             },
-            applyToSelectionOn: function(controls,controlKey,event,extra){
+            applyToSelectionOn: function(controls, controlKey, event, extra) {
                 var imageGrid = this;
-                controls[controlKey].on(event,function(){
+                controls[controlKey].on(event, function() {
                     imageGrid._settingsTab.setChanged(controls[controlKey]);
-                    if(imageGrid._settingsTab.isPreview()){
-                        if(typeof extra != "undefined"){
-                            extra.apply(this,arguments);
+                    if (imageGrid._settingsTab.isPreview()) {
+                        if (typeof extra !== "undefined") {
+                            extra.apply(this, arguments);
                         }
                         imageGrid.applyToSelection(controls);
                     }
                 });
             }
-        }
+        };
     });
-}(jQuery))
+}(jQuery));
